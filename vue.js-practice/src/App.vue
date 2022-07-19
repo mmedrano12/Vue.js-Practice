8cf5d1fc60f33e04c7264e845be131a93772c885
Matthew Medrano <medran16@students.rowan.edu>
2022-07-19 18:06:02 -0400

    first commit

---
+++ vue.js-practice/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Home/>
+</div> 
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  data () {
+    return{
+      api_key: 'd5e4d419a18fabbf6f39acd494c88391'
+    }
+  },
+  components: {
+    Home
+  }
+}
+</script>
+
+<!--CSS stuff-->
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
